fefa989213a10859b22238cdaa97d3065b22dedb
Anindya <anindyany@gmail.com>
2025-03-02 20:28:44 -0500

    3x3

---
+++ checkerboard3x3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main(){
+    int size{3};
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++){
+                cout << "*";
+            } 
+        
+            cout << endl;
+        }
+            return 0;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
